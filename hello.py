8af572065c4a8c84d57f24f5920a915112816aa7
Sudarshan Gadiyar <sudarshangadiyar@gmail.com>
2020-12-12 16:33:04 +0530

    'added comments'

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from tkinter import *
 
 root = Tk()
-
+#create a simple word label to add to the window
 myLabel = Label(root, text = "Hello world")
-
+#pack the label on to a window
 myLabel.pack()
-
+#tkinter loop has to be closed so it can give output
 root.mainloop()
